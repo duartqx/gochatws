a7f59f3d15138c677b9da8c7991429e4bda19cc4
Diego Duarte <duartqx@gmail.com>
2023-10-19 19:55:41 -0300

    Renamed package import

--- core/interfaces/Model.go
+++ core/interfaces/Model.go
@@ -1,14 +1,14 @@
 package core
 
 import (
+	e "github.com/duartqx/gochatws/core/errors"
 	"github.com/go-playground/validator/v10"
-	cerr "gochatws/core/errors"
 )
 
 type ParserFunc func(out interface{}) error
 
 type Model interface {
 	ParseAndValidate(parser ParserFunc, v *validator.Validate) (
-		Model, error, *[]cerr.ValidationErrorResponse,
+		Model, error, *[]e.ValidationErrorResponse,
 	)
 }
