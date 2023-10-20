0f66377e4a3714472f2b9f7a00a37c25dc69b66b
Diego Duarte <duartqx@gmail.com>
2023-10-20 17:49:24 -0300

    Added parser, validation and UpdateFromAnother test

--- tests/UserModel_test.go
+++ tests/UserModel_test.go
@@ -1,21 +1,56 @@
 package tests
 
 import (
-	"log"
+	"encoding/json"
 	"testing"
 
 	i "github.com/duartqx/gochatws/core/interfaces"
 	u "github.com/duartqx/gochatws/domains/auth/users"
+	"github.com/go-playground/validator/v10"
 )
 
 func TestUserModel(t *testing.T) {
 
+	v := validator.New()
+
 	var user i.User = &u.UserModel{}
-	t.Log(user)
 
 	if _, ok := user.(*u.UserModel); !ok {
 		t.Errorf("UserModel does not implements Model!")
 	}
 
-	log.Println("UserModel implements Model!")
+	parser := func(out interface{}) error {
+		jsonBody := []byte(`{
+			"name": "Other",
+			"username": "other@teste.com",
+			"password": "password"
+		}`)
+		return json.Unmarshal(jsonBody, &out)
+	}
+
+	parsedOther, err, validationErrs := u.UserModel{}.ParseAndValidate(parser, v)
+	if validationErrs != nil || err != nil {
+		t.Errorf("Could not Validate UserModel: %s", err)
+	}
+
+	if parsedOther.Username != "other@teste.com" ||
+		parsedOther.Name != "Other" ||
+		parsedOther.Password != "password" {
+
+		t.Errorf(
+			"Did not parsed correctly UserModel: %s, %s, %s",
+			parsedOther.Name,
+			parsedOther.Username,
+			parsedOther.Password,
+		)
+	}
+
+	userFromOther := u.UserModel{}
+
+	userFromOther.UpdateFromAnother(parsedOther)
+
+	if userFromOther.Username != parsedOther.Username ||
+		userFromOther.Name != parsedOther.Name {
+		t.Errorf("UpdateFromAnother did not update a User")
+	}
 }
