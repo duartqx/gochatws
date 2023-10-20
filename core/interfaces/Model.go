5c278716572d5822cc494f5b666dac2cfda55392
Diego Duarte <duartqx@gmail.com>
2023-10-20 15:33:53 -0300

    Updated Model interface to User and set it's method signatures

--- core/interfaces/Model.go
+++ core/interfaces/Model.go
@@ -1,14 +1,11 @@
-package core
-
-import (
-	e "github.com/duartqx/gochatws/core/errors"
-	"github.com/go-playground/validator/v10"
-)
+package interfaces
 
 type ParserFunc func(out interface{}) error
 
-type Model interface {
-	ParseAndValidate(parser ParserFunc, v *validator.Validate) (
-		Model, error, *[]e.ValidationErrorResponse,
-	)
+type User interface {
+	Clean() User
+	GetId() int
+	GetName() string
+	GetUsername() string
+	UpdateFromAnother(other User)
 }
