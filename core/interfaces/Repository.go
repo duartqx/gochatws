79b602600b06671b43e0ed817fbb71204429f07f
Diego Duarte <duartqx@gmail.com>
2023-10-22 23:26:17 -0300

    ChatRepository

--- core/interfaces/Repository.go
+++ core/interfaces/Repository.go
@@ -1,5 +1,13 @@
 package interfaces
 
-type Repository interface {
+type ChatRepository interface {
+	FindById(id int) (ChatRoom, error)
+	FindByParamId(id string) (ChatRoom, error)
+	ParseAndValidate(parser ParserFunc) (ChatRoom, error)
+	Create(cr ChatRoom) error
+	All() (*[]ChatRoom, error)
+}
+
+type UserRepository interface {
 	FindById(id int) (User, error)
 }
