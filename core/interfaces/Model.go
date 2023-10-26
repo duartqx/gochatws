ff28bba669e63df3c3b03825b19c10c708984c70
Diego Duarte <duartqx@gmail.com>
2023-10-26 17:13:32 -0300

    Reorder and added Message interface

--- core/interfaces/Model.go
+++ core/interfaces/Model.go
@@ -2,17 +2,6 @@ package interfaces
 
 type ParserFunc func(out interface{}) error
 
-type User interface {
-	Clean() User
-	GetId() int
-	GetName() string
-	GetPassword() string
-	GetUsername() string
-	SetId(id int)
-	SetPassword(password string)
-	UpdateFromAnother(other User)
-}
-
 type ChatRoom interface {
 	GetCategory() int
 	GetCreator() User
@@ -24,6 +13,29 @@ type ChatRoom interface {
 	SetId(id int)
 }
 
+type Message interface {
+	GetId() int
+	SetId(int) Message
+	GetChatId() int
+	GetUserId() int
+	GetText() string
+	GetUser() User
+	GetChat() ChatRoom
+	PopulateUser(user User) Message
+	PopulateChat(chat ChatRoom) Message
+}
+
 type Session interface {
 	GetToken() string
+}
+
+type User interface {
+	Clean() User
+	GetId() int
+	GetName() string
+	GetPassword() string
+	GetUsername() string
+	SetId(id int)
+	SetPassword(password string)
+	UpdateFromAnother(other User)
 }
