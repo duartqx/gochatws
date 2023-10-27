d27b0a2459eb114498ab740592378be1e2163b58
Diego Duarte <duartqx@gmail.com>
2023-10-27 01:27:40 -0300

    ChatRoomController added Chat method to render the chat template

--- domains/controllers/ChatRoomController.go
+++ domains/controllers/ChatRoomController.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 
 	e "github.com/duartqx/gochatws/core/errors"
+	i "github.com/duartqx/gochatws/core/interfaces"
 	m "github.com/duartqx/gochatws/domains/models"
 	s "github.com/duartqx/gochatws/domains/services"
 	"github.com/duartqx/gochatws/domains/utils"
@@ -29,6 +30,22 @@ func (crc ChatRoomController) All(c *fiber.Ctx) error {
 func (crc ChatRoomController) One(c *fiber.Ctx) error {
 	response := crc.chatRoomService.One(c.Params("id"))
 	return c.Status(response.Status).JSON(response.Body)
+}
+
+func (crc ChatRoomController) Chat(c *fiber.Ctx) error {
+	user, err := utils.GetUserFromLocals(c.Locals("user"))
+	if err != nil {
+		return c.Render("404", fiber.Map{"Title": "404 Not Found"})
+	}
+	response := crc.chatRoomService.One(c.Params("id"))
+	if response.Status != http.StatusOK {
+		return c.Render("404", fiber.Map{"Title": "404 Not Found"})
+	}
+	chat, ok := response.Body.(i.ChatRoom)
+	if !ok {
+		return c.Render("404", fiber.Map{"Title": "404 Not Found"})
+	}
+	return c.Render("chat", fiber.Map{"Title": chat.GetName(), "User": user})
 }
 
 func (crc ChatRoomController) Create(c *fiber.Ctx) error {
